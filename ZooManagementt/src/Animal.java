1d3d00265891bf12d8eda10678f1f61c3a0b022f
Samar Souissi <samar.souissi27@gmail.com>
2025-02-18 01:14:32 +0100

    prosit2

---
+++ ZooManagementt/src/Animal.java
@@ -0,0 +1,16 @@
+public class Animal {
+     String family;
+     String name;
+   int age;
+     boolean isMammal;
+
+     Animal(){}
+    Animal(String family, String name, int age, boolean isMammal) {
+        this.family = family;
+        this.name = name;
+        this.age = age;
+        this.isMammal = isMammal;
+    }
+
+
+}
---
+++ ZooManagementt/src/Zoo.java
@@ -0,0 +1,61 @@
+public class Zoo {
+    ;
+    Animal[] animals;
+     String name;
+     String city;
+    int nbrAnimal;
+
+    Zoo(){}
+    Zoo(String name, String city ) {
+        this.name = name;
+        this.city = city;
+      this.animals = new Animal[25];
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("City: " + city);
+        System.out.println("Nbr Cages: " + nbrAnimal);
+    }
+
+    int searchAnimal(Animal animal) {
+        int index = -1;
+        for (int i = 0; i < nbrAnimal; i++) {
+            if (animal.name == animals[i].name)
+                return i;
+        }
+        return index;
+    }
+    boolean addAnimal(Animal animal) {
+        if (searchAnimal(animal) != -1)
+            return false;
+        if (nbrAnimal == 25)
+            return false;
+        animals[nbrAnimal] = animal;
+        nbrAnimal++;
+        return true;
+    }
+
+    boolean removeAnimal(Animal animal) {
+        int indexAnimal = searchAnimal(animal);
+        if (indexAnimal == -1)
+            return false;
+        for (int i = indexAnimal; i < nbrAnimal; i++) {
+            animals[i] = animals[i + 1];
+        }
+        this.nbrAnimal--;
+        animals[nbrAnimal] = null;
+        return true;
+    }
+
+
+    void displayAnimals() {
+        System.out.println("List of animals of " + name + ":");
+        for (int i = 0; i < nbrAnimal; i++) {
+            System.out.println(animals[i]);
+        }
+    }
+
+
+
+}
